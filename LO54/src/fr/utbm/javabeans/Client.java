6918250399050f566ae5b45073da9f1acbcfbcbc
NaifousBS <soufian1993@hotmail.fr>
2016-11-04 18:19:00 +0100

    Update bean Client

--- LO54/src/fr/utbm/javabeans/Client.java
+++ LO54/src/fr/utbm/javabeans/Client.java
@@ -1,7 +1,59 @@
 package fr.utbm.javabeans;
 
+/**
+ * The Class Client.
+ */
 public class Client {
 
+	/** The id. */
 	private int id;
-	Kushadaci
+	
+	/** The lastname. */
+	private String lastname;
+	
+	/** The firstname. */
+	private String firstname;
+	
+	/** The address. */
+	private String address;
+	
+	/** The phone. */
+	private int phone;
+	
+	/** The email. */
+	private String email;
+	
+	/** The course session id. */
+	private int courseSessionId;
+	
+	/**
+	 * Instantiates a new client.
+	 */
+	public Client() {
+	}
+
+	/**
+	 * Instantiates a new client.
+	 *
+	 * @param id the id
+	 * @param lastname the lastname
+	 * @param firstname the firstname
+	 * @param address the address
+	 * @param phone the phone
+	 * @param email the email
+	 * @param courseSessionId the course session id
+	 */
+	public Client(int id, String lastname, String firstname, String address, int phone, String email,
+			int courseSessionId) {
+		super();
+		this.id = id;
+		this.lastname = lastname;
+		this.firstname = firstname;
+		this.address = address;
+		this.phone = phone;
+		this.email = email;
+		this.courseSessionId = courseSessionId;
+	}
+	
+	
 }
